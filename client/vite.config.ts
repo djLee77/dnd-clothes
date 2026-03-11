53ad283fecd4923ca9e29e1f9ad4efb3c6b96409
djLee77 <dkdnl232@gmail.com>
2026-03-11 23:48:10 +0900

    feat : react compiler 적용

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ReactCompilerConfig = {
+  target: '18', // React 18 호환 설정
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['babel-plugin-react-compiler', ReactCompilerConfig],
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
